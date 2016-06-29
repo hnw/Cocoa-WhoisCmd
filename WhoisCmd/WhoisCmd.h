8bcacc9cb5ca39dc6e5883d56914a08bfc6bac97
Yoshio HANAWA <y@hnw.jp>
2016-06-30 00:06:08 +0900

    Initial commit

---
+++ WhoisCmd/WhoisCmd.h
@@ -0,0 +1,19 @@
+//
+//  WhoisCmd.h
+//  WhoisCmd
+//
+//  Created by hnw on 2016/06/30.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhoisCmd.
+FOUNDATION_EXPORT double WhoisCmdVersionNumber;
+
+//! Project version string for WhoisCmd.
+FOUNDATION_EXPORT const unsigned char WhoisCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhoisCmd/PublicHeader.h>
+
+
